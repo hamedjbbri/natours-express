b0caf1887272669e977edf52b2b63bf047155217
Hamed <jabbary.hamed@gmail.com>
2023-10-22 17:10:27 -0700

    change

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// middleware
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'hello from the server', app: 'natours' });
 // });
@@ -16,6 +19,7 @@ const port = 3000;
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -24,6 +28,28 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'created',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
