856200aa34fa58530dad6a2b0f80e186aef26d1a
Hamed <jabbary.hamed@gmail.com>
2023-11-11 20:01:26 -0800

    user roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
+    role: req.body.role,
   });
 
   const token = signToken(newUser._id);
@@ -98,4 +99,16 @@ exports.protect = async (req, res, next) => {
   req.user = freshUser;
 
   next();
+};
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        status: 'error',
+        message: 'you have no permission for this action',
+      });
+    }
+    next();
+  };
 };
