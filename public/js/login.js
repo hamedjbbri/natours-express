04316398151ca8a009b7015c6eeb032a419748b7
Hamed <jabbary.hamed@gmail.com>
2023-12-09 14:58:01 -0800

    authintication

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,23 @@
-const login = async (email, password) => {
-  console.log(email, password);
+import { showAlert } from './alerts';
+
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'post',
       url: 'http://localhost:8000/api/v1/users/login',
       data: { email, password },
     });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'logged in successfully');
+
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+
     console.log(res);
   } catch (error) {
-    console.log(error.response.data);
+    showAlert('error', error.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
